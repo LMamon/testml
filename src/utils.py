ca9936e0911664ace84370499889e20b1b5bf9e4
Louis M <knit-close.0o@icloud.com>
2025-12-22 21:18:18 -0900

    fix: updated column names

--- src/utils.py
+++ src/utils.py
@@ -3,25 +3,25 @@ import pandas as pd
 def load_standard_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     allowed_columns = [
-        'Attendence',
-        'Midterm',
-        'Final',
-        'Projects',
-        'Study_Hours'
+        'Attendance (%)',
+        'Midterm_Score',
+        'Final_Score',
+        'Projects_Score',
+        'Study_Hours_per_Week'
     ]
     return df[allowed_columns]
 
 def load_biased_data(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     biased_allowed_columns = [
-        'Attendence',
-        'Midterm',
-        'Final',
-        'Project',
-        'Study_Hours',
+        'Attendance (%)',
+        'Midterm_Score',
+        'Final_Score',
+        'Projects_Score',
+        'Study_Hours_per_Week',
         'Gender',
-        'Parental_Level_of_Education',
-        'Internet_Access'
+        'Parent_Education_Level',
+        'Internet_Access_at_Home'
     ]
 
     return df[biased_allowed_columns]
